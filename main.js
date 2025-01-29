701ebfd88bd6437cfbeb927f550087618d3ac950
mewo? <159087398+ieatsoulsmeow@users.noreply.github.com>
2025-01-29 15:30:14 -0600

    Dark Mode Added

--- main.js
+++ main.js
@@ -1,9 +1,28 @@
-document.addEventListener("click", (event) => {
-    if (event.target.id === "themeToggle") {
-        updateTheme();
+document.addEventListener("DOMContentLoaded", () => {
+    const themeToggle = document.getElementById("themeToggle");
+    const darkModeIcon = document.getElementById("themeToggle").firstElementChild;
+    const body = document.body;
+
+    //Loads theme
+    const savedTheme = localStorage.getItem("theme");
+    if (savedTheme) {
+        toggleDarkMode(body, darkModeIcon)
     }
+
+    //Toggles theme
+    themeToggle.addEventListener("click", () => {
+        toggleDarkMode(body, darkModeIcon)
+    });
 });
 
-function updateTheme() {
-    
+function toggleDarkMode(body, darkModeIcon) {
+    if (body.classList.contains("dark")) {
+        body.classList.remove("dark");
+        localStorage.setItem("theme", ""); //Save user preference
+        darkModeIcon.src = "assets/icons/light-mode.svg";
+    } else {
+        body.classList.add("dark");
+        localStorage.setItem("theme", "dark"); //Save user preference
+        darkModeIcon.src = "assets/icons/dark-mode.svg";
+    }
 }
